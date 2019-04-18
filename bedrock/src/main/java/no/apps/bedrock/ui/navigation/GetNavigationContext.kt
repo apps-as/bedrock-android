8eaa35db74bf25d82732e447396ff42f6cd498db
Mate Herber <mate.herber@outlook.com>
2019-04-18 15:35:31 +0200

    Fixed added nullability on navigation context provider

--- bedrock/src/main/java/no/apps/bedrock/ui/navigation/GetNavigationContext.kt
+++ bedrock/src/main/java/no/apps/bedrock/ui/navigation/GetNavigationContext.kt
@@ -1,5 +1,5 @@
 package no.apps.bedrock.ui.navigation
 
 interface GetNavigationContext {
-    operator fun invoke(currentPageArgs: PageArgs?, nextPageArgs: PageArgs): NavigationContext
+    operator fun invoke(currentPageArgs: PageArgs, nextPageArgs: PageArgs): NavigationContext
 }
