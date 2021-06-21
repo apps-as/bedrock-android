b2b42bcfc24c41de1685c299c7779912c05c9763
Balázs Rozsenich <rozsenichb@Nostromo.local>
2021-06-21 10:45:47 +0200

    Upgrade dependencies and remove jcenter repository

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -10,7 +10,7 @@ object AndroidConfig {
     val sourceCompatibility = JavaVersion.VERSION_1_8
     val targetCompatibility = JavaVersion.VERSION_1_8
     const val versionCode = 1
-    const val versionName = "2.1.2"
+    const val versionName = "2.2.0"
 }
 
 object Libs {
@@ -42,13 +42,13 @@ object Libs {
 }
 
 private const val androidMavenPublishVersion = "3.6.3"
-private const val buildToolsVersion = "4.1.2"
+private const val buildToolsVersion = "4.2.1"
 private const val conductorVersion = "3.0.1"
-private const val coreVersion = "1.3.2"
-private const val dagger2Version = "2.33"
-private const val dependenciesVersion = "0.36.0"
-private const val kotlinCoroutinesVersion = "1.4.2"
-private const val kotlinVersion = "1.4.31"
-private const val lifecycleVersion = "2.3.0"
+private const val coreVersion = "1.6.0-rc01"
+private const val dagger2Version = "2.37"
+private const val dependenciesVersion = "0.39.0"
+private const val kotlinCoroutinesVersion = "1.5.0"
+private const val kotlinVersion = "1.5.10"
+private const val lifecycleVersion = "2.3.1"
 private const val lifecycleExtensionsVersion = "2.2.0"
 private const val timberVersion = "4.7.1"
